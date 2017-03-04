c234d7f28dc01c310cc1da2e28c95e0b06151c7d
Ajit Kumar <er.ajitkumar90@gmail.com>
2017-03-04 17:43:11 +0530

    change

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var express = require("express");
-var app = express();                     //instantiates Express
+var app = express(); //instantiates Express
 var http = require("http");
 var server = require("http").createServer(app);
-var bodyParser = require("body-parser");   //bodyParser is in fact the composition of three middlewares like json, urlencoded and multipart
+var bodyParser = require("body-parser"); //bodyParser is in fact the composition of three middlewares like json, urlencoded and multipart
 var chalk = require('chalk');
 var morgan = require('morgan');
 var config_mode = require('config');
 //var cors = require('cors');
 
 //Set ENV and get app settings
-process.env.NODE_ENV = process.env.NODE_ENV || app.get('env')
+// process.env.NODE_ENV = process.env.NODE_ENV || app.get('env');
+if (process.env.PORT) {
+    process.env.PORT = process.env.PORT;
+    process.env.NODE_ENV = "production";
+} else {
+    process.env.PORT = 7000;
+    process.env.NODE_ENV = "production";
+}
 global.config = config_mode.get(process.env.NODE_ENV);
-process.env.PORT = config.port;
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -19,10 +25,9 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With, access_token');
 
     if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
+        res.sendStatus(200);
+    } else {
+        next();
     }
 };
 
@@ -32,7 +37,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 // app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(allowCrossDomain);
@@ -45,6 +50,3 @@ require("./routes")(app, router);
 server.listen(process.env.PORT, function() {
     console.log(chalk.bgBlue("Alcon server is running  at http://" + server.address().address + ":" + server.address().port + ' in ') + chalk.bgGreen(process.env.NODE_ENV + ' Mode.'));
 });
-
-
-
